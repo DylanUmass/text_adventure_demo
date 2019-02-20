67fdec7133f31c15ae2d5a55cd0d12bd1b525d1c
mrwalker998 <47701439+mrwalker998@users.noreply.github.com>
2019-02-20 13:15:22 -0500

    Update items.c

--- items.c
+++ items.c
@@ -3,41 +3,39 @@
 #include <string.h>
 #include "items.h"
 
-//Creates an item with the given name and description, and returns a pointer to it.
-item* createItem(char name[25], char desciption[100]) {
-	item* itmPnt = (item*) malloc(sizeOf(item));
+//Creates an item with the given name and description, and returns it.
+item createItem(char* name, char* description) {
 	item newItem = {name, description};
-	itmPnt[0] = newItem;
-	return itmPnt;
+	return newItem;
 }
 
+//Null item. Returned by functions that return an item when there is an error.
+item nullItem = {"", ""};
+
 //Creates an inventory with the given name and max size, and dynamically allocates memory for items.
-inventory* createInv(char name[25], int maxSize) {
-	item* items[maxSize];
-	for(int i = 0; i < maxSize; i++) {
-		items[i] = (item*) malloc(sizeOf(item*));
-	}
-	inventory* invPnt = (inventory*) malloc(sizeOf(inventory));
-	inventory newInv = {name, 0, maximumSize, items};
-	invPnt[0] = newInv;
+inventory* createInv(char* name, int maxSize) {
+	item* items = (item*) malloc(maxSize * sizeof(item));
+	inventory* invPnt = (inventory*) malloc(sizeof(inventory));
+	inventory newInv = {name, 0, maxSize, items};
+	(*invPnt) = newInv;
 	return invPnt;
 }
 
 //Compares a name to an item. Returns 0 if the item has that name.
-_Bool compareItem(char[] name, item* itm) {
-	return strcmp(name, itm -> name);
+_Bool compareItem(char* name, item itm) {
+	return strcmp(name, itm.name);
 }
 
 //Compares a name to an inventory. Returns 0 if the inventory has that name.
-_Bool compareInv(char[] name, inventory* inv) {
+_Bool compareInv(char* name, inventory* inv) {
 	return strcmp(name, inv -> name);
 }
 
 //Checks to see if an item with the given name exists in the given inventory. Returns the index if so, otherwise -1.
-int contains(char[] name, inventory* inv) {
+int contains(char* name, inventory* inv) {
 	for(int i = 0; i < inv -> numItems; i++) {
-		item currItem* = inv -> items[i];
-		if(compareItems(name, currItem) == 0) {
+		item currItem = inv -> items[i];
+		if(compareItem(name, currItem) == 0) {
 			return i;
 		} 
 	}
@@ -45,65 +43,94 @@ int contains(char[] name, inventory* inv) {
 }
 
 //Removes an item with the given name from the given inventory. Returns 0 if successful, or -1 if the item wasn't found in the inv.
-int removeItem(char[] name, inventory* inv) {
+int removeItem(char* name, inventory* inv) {
 	int index = contains(name, inv);
-	if(containsItem == -1) {return -1;}
+	if(index == -1) {return -1;}
 	else {
-		for(int i = index; i < inv -> numItems - 1; i++) {
+		for(int i = index; i < inv -> numItems - 2; i++) {
 			inv -> items[i] = inv -> items[i + 1];
 		}
-		inv -> items[numItems] == NULL;
 		inv -> numItems--;
+		inv -> items[inv -> numItems] = nullItem;
 	}
 	return 0;
 }
 
 //Adds a given item to the given inventory. Returns 0 if it was added successfully, or -1 if inv is full.
-int addItem(item* itm, inventory* inv) {
+int addItem(item itm, inventory* inv) {
 	if(inv -> numItems == inv -> maximumSize) {
 		return -1;
 	}
 	else {
-		inv -> items[numItems] = itm;
+		inv -> items[inv -> numItems] = itm;
 		inv -> numItems++;
 	}
 	return 0;
 }
 
 //Prints an item in the form <itemname>: <itemdescription>
-void printItem(item* itm) {
-	prinf(%s: %s, itm -> name, itm -> description);
+void printItem(item itm) {
+	printf("%s: %s\n", itm.name, itm.description);
 }
 
 //Prints all of the items in the given inventory in the form <itemname>: <itemdescription>
 void printInventory(inventory* inv) {
-	for(int i = 0; i < inv -> numItems, i++) {
+	printf("%s's Contents:\n\n", inv -> name);
+	for(int i = 0; i < inv -> numItems; i++) {
+		printf("%i. ", i + 1);
 		printItem(inv -> items[i]);
-		printf("\n");
 	}
 }
 
 //Returns an item from an inventory with the given name, or NULL if that item doesn't exist in the inventory.
-item* getItem(char[] name, inventory* inv) {
+item getItem(char* name, inventory* inv) {
 	int index = contains(name, inv);
 	if(index == -1) {
-		return NULL;
+		return nullItem;
 	}
 	return inv -> items[index];
 }
 
 //Given a name, returns an item from the given inventory with that name, and then removes that item. Returns NULL if item not found.
-item* takeItem(char[] name, inventory* inv) {
-	item* itm = getItem(name, inv);
-	if(itm == NULL) {return NULL;}
+item takeItem(char* name, inventory* inv) {
+	item itm = getItem(name, inv);
+	if(compareItem("", itm) == 0) {return nullItem;}
 	removeItem(name, inv);
 	return itm;
 }
 
 //Frees an inventory's location in memory, along with all its associated items.
 void freeInventory(inventory* inv) {
-	for(int i = 0; i < inv -> maximumSize; i++) {
-		free(inv -> items[i]);
-	}
+	free(inv -> items);
 	free(inv);
+}
+
+int main() {
+	item sword = createItem("Silver Sword", "A large silver sword with a ruby.");
+	item potion = createItem("Blue Potion", "A blue potion in a tall glass beaker.");
+	item key = createItem("Rusty Key", "A rusty iron key to a jail lock.");
+	inventory* chest = createInv("Wooden Chest", 10);
+	addItem(sword, chest);
+	addItem(potion, chest);
+	addItem(key, chest);
+	printInventory(chest);
+	printf("--------\n");
+	item keyAlias = takeItem("Rusty Key", chest);
+	item invalid = takeItem("Rusty Key", chest);
+	printItem(keyAlias);
+	printf("--------\n");
+	printInventory(chest);
+	printf("--------\n");
+	removeItem("Blue Potion", chest);
+	for(int i = 0; i < 20; i++) {
+		addItem(sword, chest);
+	}
+	printInventory(chest);
+	printf("--------\n");
+	for(int i = 0; i < 10; i++) {
+		removeItem("Silver Sword", chest);
+	}
+	printInventory(chest);
+	freeInventory(chest);
+	return 0;
 }
