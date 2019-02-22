92ae3c3cdc4b8be45e19d52fc7bee46c74e9a8b1
mrwalker998 <47701439+mrwalker998@users.noreply.github.com>
2019-02-22 12:37:09 -0500

    Added a player struct and a function to create one. Hasn't been tested yet.

--- adventure.c
+++ adventure.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "rooms.h"
 #include "items.h"
+
+//A struct to represent the player. Has a field for a name that the player chooses, an inventory, and a current room.
+struct Player {
+	char* name;
+	Inventory* pInv;
+	Room* currentRoom;
+}
+
+//Typedefs
+typedef struct Player Player;
+
+//Returns a pointer to a player struct with the name the user types. 
+Player* createPlayer() {
+	char* name;
+	printf("\n\nPlease enter your name: ");
+	scanf("%s", name);
+	Player* newPlayerPnt = (Player*) malloc(sizeof(Player));
+	char* namecpy;
+	strcpy(namecpy, name);
+	Player newPlayer = {name, createInv(strcat(namecpy, "'s Inventory"), 25), NULL}; //Player can hold max 25 items
+	(*newPlayerPnt) = newPlayer;
+	return newPlayerPnt;
+}
 
 //Initialize the game's 8 Rooms and return an array of the 8 rooms
 Room **resetRooms() {
