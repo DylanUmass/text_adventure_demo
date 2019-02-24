6ec88e07c466e0a2e3b88cd7f892fbc94e5a9fa1
mrwalker998 <47701439+mrwalker998@users.noreply.github.com>
2019-02-24 15:51:13 -0500

    Added some more functions for the player struct.

--- adventure.c
+++ adventure.c
@@ -17,7 +17,7 @@ typedef struct Player Player;
 //Returns a pointer to a player struct with the name the user types. 
 Player* createPlayer() {
 	char* name;
-	printf("\n\nPlease enter your name: ");
+	printf("\nPlease enter your name: ");
 	scanf("%s", name);
 	Player* newPlayerPnt = (Player*) malloc(sizeof(Player));
 	char* namecpy;
@@ -25,6 +25,39 @@ Player* createPlayer() {
 	Player newPlayer = {name, createInv(strcat(namecpy, "'s Inventory"), 25), NULL}; //Player can hold max 25 items
 	(*newPlayerPnt) = newPlayer;
 	return newPlayerPnt;
+}
+
+//Lets the player view their inventory.
+void playerViewInventory(Player* player) {
+	printf("\n\n");
+	printInventory(player -> pInv);
+}
+
+void playerViewRoomItems(Player* player) {
+	printf("\n\n");
+	printInventory(player -> currentRoom -> items);
+}
+
+//Lets the player take an item from their current room by entering the name of it.
+void playerTakeItem(Player* player) {
+	char* itemName;
+	printf("\nSelect an item to take: ");
+	scanf("%s", itemName);
+	int result = swapItem(itemName, player -> currentRoom -> items, player -> pInv);
+	if(result == -1) {
+		printf("\nNo item in the room with that name.");
+	}
+}
+
+//Lets the player drop an item from their inventory into their current room by entering the name of it.
+void playerDropItem(Player* player) {
+	char* itemName;
+	printf("\nSelect an item to drop: ");
+	scanf("%s", itemName);
+	int result = swapItem(itemName, player -> pInv, player -> currentRoom -> items);
+	if(result == -1) {
+		printf("\nYou don't have that item.");
+	}
 }
 
 //Initialize the game's 8 Rooms and return an array of the 8 rooms
